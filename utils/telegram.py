3aeb6ba95b23f6f13bef2dba35aec33d3f7a44f3
Kalash575 <83688609+Kalash575@users.noreply.github.com>
2025-07-25 12:23:21 +0530

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -1,7 +1,7 @@
 import requests
 
 def send_signal(msg):
-    TOKEN = "your_bot_token"
-    CHAT_ID = "your_chat_id"
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={msg}"
+    TOKEN = "7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y"
+    CHAT_ID = "5601858231"
+    url = f"https://api.telegram.org/bot{7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y}/sendMessage?chat_id={5601858231}&text={msg}"
     requests.get(url)
