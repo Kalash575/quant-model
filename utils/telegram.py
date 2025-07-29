4f8b107f916020f2a1c85cbd0f98804204f4352a
Kalash575 <83688609+Kalash575@users.noreply.github.com>
2025-07-29 15:14:56 +0530

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -6,6 +6,14 @@ def send_signal(msg):
     url = f"https://api.telegram.org/bot{7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y}/sendMessage?chat_id={5601858231}&text={msg}"
     requests.get(url)
 
+# Check current IST time
+def is_market_closed():
+    ist = pytz.timezone('Asia/Kolkata')
+    now = datetime.now(ist).time()
+    market_close_time = time(15, 30)  # 3:30 PM IST
+    return now >= market_close_time
+
+
 # Send goodbye message
 def send_goodbye_message():
     message = "📉 Market band hogaya bhai, kal milte hai 🚪 — Goobye Trader"
