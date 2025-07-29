a53fe2a906aa8d6c0ade080a2fab136b083bcfe3
Kalash575 <83688609+Kalash575@users.noreply.github.com>
2025-07-29 15:20:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,26 @@ while True:
         print("⚠️ Error:", e)
 
     time.sleep(300)  # Wait 5 minutes before next run
+
+# Check current IST time
+def is_market_closed():
+    ist = pytz.timezone('Asia/Kolkata')
+    now = datetime.now(ist).time()
+    market_close_time = time(15, 30)  # 3:30 PM IST
+    return now >= market_close_time
+
+
+# Send goodbye message
+def send_goodbye_message():
+    message = "📉 Market band hogaya bhai, kal milte hai 🚪 — Goobye Trader"
+    url = f"https://api.telegram.org/bot{7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y}/sendMessage"
+    payload = {
+        'chat_id': 5601858231,
+        'text': message,
+        'parse_mode': 'Markdown'
+    }
+    requests.post(url, data=payload)
+
+# Run check and send
+if is_market_closed():
+    send_goodbye_message()
