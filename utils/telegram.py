148a78f263ca67cec3cbd370cafba47fd3e9733f
Kalash575 <83688609+Kalash575@users.noreply.github.com>
2025-07-29 15:14:08 +0530

    Update telegram.py

--- utils/telegram.py
+++ utils/telegram.py
@@ -5,3 +5,18 @@ def send_signal(msg):
     CHAT_ID = "5601858231"
     url = f"https://api.telegram.org/bot{7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y}/sendMessage?chat_id={5601858231}&text={msg}"
     requests.get(url)
+
+# Send goodbye message
+def send_goodbye_message():
+    message = "📉 Market band hogaya bhai, kal milte hai 🚪 — Goobye Trader"
+    url = f"https://api.telegram.org/bot{7494189301:AAEjSatA69sp0FxJZCByesBR8nL-CQb9i_Y}/sendMessage"
+    payload = {
+        'chat_id': 5601858231,
+        'text': message,
+        'parse_mode': 'Markdown'
+    }
+    requests.post(url, data=payload)
+
+# Run check and send
+if is_market_closed():
+    send_goodbye_message()
